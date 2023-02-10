f76cb31b74209ba66d50c05bf74b7b102ca0fc8f
udit <udit@udits-MacBook-Air.local>
2023-02-10 15:04:26 +0530

    added error prevention statements

--- app.js
+++ app.js
@@ -18,10 +18,13 @@ yargs.command({
     }
 })
 
+
+
 const getWeather = function(address) {
     const params = {
     access_key: 'ad6d5c569fd48c67076910e08e7b2e41',
-    query: address
+    query: address,
+    limit: 1
     }
     let latitude =''
     let longitude =''
@@ -32,14 +35,24 @@ const getWeather = function(address) {
         latitude = response.data.data[0].latitude
         longitude = response.data.data[0].longitude
         url = 'http://api.weatherstack.com/current?access_key=6459f02bbb7911dce62287ec08544885&query='+latitude+','+longitude+'&units=m'
+        setTimeout(()=>{request({ url: url, json: true },(error, response) => {
+            if (error) {
+                console.log('Could not connect to web services!')
+            } else if(response.body.error){
+                console.log('Please enter valid address!')
+            }
+            else {
+                const temp = response.body.current.temperature
+            console.log(response.body.current.weather_descriptions[0]+'. It is currently '+chalk.yellow.inverse(temp)+' degree celcius in '+ response.body.location.name)
+            }
+        } )},2000)
     }).catch(error => {
-        console.log(error);
+        if (error.code === 'ERR_BAD_REQUEST') {
+            console.log('Enter valid address')
+        }else if (error.code === 'ENOTFOUND') {
+            console.log('Could not connect to web services!');   
+        }
     });
-
-    setTimeout(()=>{request({ url: url, json: true },(error, response) => {
-        const temp = response.body.current.temperature
-        console.log(response.body.current.weather_descriptions[0]+'. It is currently '+chalk.yellow.inverse(temp)+' degree celcius in '+ response.body.location.name)
-    } )},2000)
 }
 
 yargs.parse()
